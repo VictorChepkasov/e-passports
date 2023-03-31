595131c0a0d45ad289ea1a8447cecb226ddeb936
Victor Chepkasov <92711919+SugaryVictor@users.noreply.github.com>
2023-03-31 10:36:59 +0500

    add ePassport struct

---
+++ contracts/passports_factory.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >= 0.8.0 <=0.8.19;
+
+contract Factory {
+    uint totalPassports;
+    // uint totalPersons; - эта перменная была бы полезна для переписи населения
+
+
+}
